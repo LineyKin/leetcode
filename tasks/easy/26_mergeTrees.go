2412ae434b7cd4a1569b832a3142433f7df86af2
sam <s.yablokov@telecontact.ru>
2024-12-12 20:46:26 +0300

    MergeTrees leetcode version

--- tasks/easy/26_mergeTrees.go
+++ tasks/easy/26_mergeTrees.go
@@ -2,6 +2,7 @@ package easy
 
 // https://leetcode.com/problems/merge-two-binary-trees/
 
+// my version
 func MergeTrees(root1 *TreeNode, root2 *TreeNode) *TreeNode {
 	if root1 == nil && root2 == nil {
 		return nil
@@ -35,4 +36,20 @@ func MergeTrees(root1 *TreeNode, root2 *TreeNode) *TreeNode {
 
 	return root1
 
+}
+
+// leetcode version
+func MergeTrees2(root1 *TreeNode, root2 *TreeNode) *TreeNode {
+	if root1 == nil {
+		return root2
+	}
+	if root2 == nil {
+		return root1
+	}
+
+	root1.Val += root2.Val
+	root1.Left = MergeTrees2(root1.Left, root2.Left)
+	root1.Right = MergeTrees2(root2.Right, root1.Right)
+
+	return root1
 }
