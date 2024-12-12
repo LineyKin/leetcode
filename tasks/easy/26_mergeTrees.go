9188b7895071101c39cfa7780830b0a930f80cad
sam <s.yablokov@telecontact.ru>
2024-12-12 20:43:49 +0300

    MergeTrees my version

---
+++ tasks/easy/26_mergeTrees.go
@@ -0,0 +1,38 @@
+package easy
+
+// https://leetcode.com/problems/merge-two-binary-trees/
+
+func MergeTrees(root1 *TreeNode, root2 *TreeNode) *TreeNode {
+	if root1 == nil && root2 == nil {
+		return nil
+	}
+
+	if root1 == nil && root2 != nil {
+		return root2
+	}
+
+	if root1 != nil && root2 == nil {
+		return root1
+	}
+
+	root1.Val += root2.Val
+
+	if root1.Left != nil {
+		root1.Left = MergeTrees(root1.Left, root2.Left)
+	}
+
+	if root1.Right != nil {
+		root1.Right = MergeTrees(root1.Right, root2.Right)
+	}
+
+	if root1.Left == nil && root2.Left != nil {
+		root1.Left = root2.Left
+	}
+
+	if root1.Right == nil && root2.Right != nil {
+		root1.Right = root2.Right
+	}
+
+	return root1
+
+}
